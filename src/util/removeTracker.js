2547330c31e95d70aed4bd040b9b977eedcb007b
baseplate77 <baseplate8087@gmail.com>
2021-09-21 21:32:57 +0530

    fixes

--- src/util/removeTracker.js
+++ src/util/removeTracker.js
@@ -2,6 +2,7 @@
 module.exports.removeTracker = (hostname, url) => {
   switch (hostname.split(".")[0]) {
     case "youtube":
+      if (url.includes("&")) return url.split("&")[0];
       return url;
     default:
       return url.split("?")[0];
