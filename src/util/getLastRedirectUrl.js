707f4f7178b813816018cef3cbd36de5f978ddca
baseplate77 <baseplate8087@gmail.com>
2021-09-21 22:19:08 +0530

    fixes

--- src/util/getLastRedirectUrl.js
+++ src/util/getLastRedirectUrl.js
@@ -1,5 +1,6 @@
 ﻿// const getredirectUrl =
 const followRedirect = require("follow-redirect-url");
+const { handleRedirectToLogin } = require("./handleRedirectToLogin");
 
 module.exports.getLastRedirectUrl = async (url) => {
   try {
@@ -8,7 +9,10 @@ module.exports.getLastRedirectUrl = async (url) => {
       //   "https://www.instagram.com/reel/CS9FBPVjl8z/?utm_source=ig_web_button_share_sheet"
     );
     console.log("urls", urls);
-    return urls[urls.length - 1]["url"];
+    const redirectUrl = urls[urls.length - 1]["url"];
+    if (redirectUrl.includes("login/") || redirectUrl.includes("account/"))
+      return urls[urls.length - 2]["url"];
+    return redirectUrl;
   } catch (e) {
     console.log(e);
     throw e;
--- src/util/removeTracker.js
+++ src/util/removeTracker.js
@@ -1,8 +1,10 @@
 ﻿// add hostname to handle to url accordingly
 module.exports.removeTracker = (hostname, url) => {
-  switch (hostname.split(".")[0]) {
-    case "youtube":
+  switch (hostname) {
+    case "youtube.com":
       if (url.includes("&")) return url.split("&")[0];
+      return url;
+    case "play.google.com":
       return url;
     default:
       return url.split("?")[0];
