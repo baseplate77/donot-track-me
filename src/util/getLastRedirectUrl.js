b2817b179d9b20664d472d35bddb7920245ca31e
baseplate77 <baseplate8087@gmail.com>
2021-09-22 22:22:55 +0530

    fixss

--- src/util/getLastRedirectUrl.js
+++ src/util/getLastRedirectUrl.js
@@ -5,13 +5,13 @@ module.exports.getLastRedirectUrl = async (url) => {
   try {
     const urls = await followRedirect.startFollowing(url);
     console.log("urls", urls);
-    const redirectUrl = urls[urls.length - 1]["url"];
-
     let redirectUrlIndex = urls.length - 1;
+    let redirectUrl = urls[redirectUrlIndex]["url"];
 
     // if the last redirect url is login than return second last url
     while (isRestricUrls(redirectUrl) && redirectUrlIndex != 0) {
       redirectUrlIndex--;
+      redirectUrl = urls[redirectUrlIndex]["url"];
     }
     console.log("redirect index", redirectUrlIndex);
     return urls[redirectUrlIndex]["url"];
